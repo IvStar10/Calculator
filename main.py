6ab0d3eccda854e1872ffa40a566c6c82d8acc82
IvStar10 sch <--no-email-->
2026-03-02 10:59:16 +0000

    Writed handling for keyboard buttons pressing.

--- main.py
+++ main.py
@@ -3,25 +3,48 @@ import tkinter as tk
 from tkinter import ttk
 
 
-def handle_operation(button_text: str):
-    def inner():
-        print(button_text)
+def handle_operation(button_text: str) -> None:
+    def inner(keypress_event=None):
+        if keypress_event is None:
+            # Нажата экранная кнопка, с клавиатуры ничего нет.
+            key = None
+        else:
+            key = keypress_event.keysym
+
+        if button_text == '=' or key == 'Return':
+            ...
+        elif button_text == 'C' or key == 'Delete':
+            enter_field.delete(0, 'end')
+        else:
+            ...
     return inner
 
 
-def handle_number(button_text: str):
-    def inner():
-        print(button_text)
+def handle_number(button_text: str) -> None:
+    def inner(keypress_event=None):
+        enter_field.insert('end', button_text)
     return inner
 
 
 CalcButton = namedtuple('CalcButton',
-                        ['column', 'row', 'text', 'is_operation'])
+                        ['column', 'row', 'text', 'is_operation', 'key'])
 buttons = (
-    CalcButton(0, 1, '7', False), CalcButton(1, 1, '8', False), CalcButton(2, 1, '9', False), CalcButton(3, 1, 'X', True),
-    CalcButton(0, 2, '4', False), CalcButton(1, 2, '5', False), CalcButton(2, 2, '6', False), CalcButton(3, 2, ':', True),
-    CalcButton(0, 3, '1', False), CalcButton(1, 3, '2', False), CalcButton(2, 3, '3', False), CalcButton(3, 3, '+', True),
-    CalcButton(0, 4, '^', True), CalcButton(1, 4, '0', False), CalcButton(2, 4, '-', True), CalcButton(3, 4, '=', True)
+    CalcButton(0, 3, '1', False, '<KP_1>'),
+    CalcButton(1, 3, '2', False, '<KP_2>'),
+    CalcButton(2, 3, '3', False, '<KP_3>'),
+    CalcButton(0, 2, '4', False, '<KP_4>'),
+    CalcButton(1, 2, '5', False, '<KP_5>'),
+    CalcButton(2, 2, '6', False, '<KP_6>'),
+    CalcButton(0, 1, '7', False, '<KP_7>'),
+    CalcButton(1, 1, '8', False, '<KP_8>'),
+    CalcButton(2, 1, '9', False, '<KP_9>'),
+    CalcButton(1, 4, '0', False, '<KP_0>'),
+    CalcButton(3, 1, 'X', True, '<KP_Multiply>'),
+    CalcButton(3, 2, ':', True, '<KP_Divide>'),
+    CalcButton(3, 3, '+', True, '<KP_Add>'),
+    CalcButton(3, 4, '-', True, '<KP_Subtract>'),
+    CalcButton(2, 4, '=', True, '<Return>'),
+    CalcButton(0, 4, 'C', True, '<Delete>')
 )
 
 window = tk.Tk()
@@ -40,8 +63,10 @@ history_text_field.pack()
 enter_field.grid(row=0, column=0, columnspan=4)
 
 for button in buttons:
-    ttk.Button(master=calc_frame, text=button.text,
-               command=(handle_operation(button.text) if button.is_operation else handle_number(button.text))).grid(row=button.row, column=button.column)
+    btn = ttk.Button(master=calc_frame, text=button.text,
+               command=(handle_operation(button.text) if button.is_operation else handle_number(button.text)))
+    btn.grid(row=button.row, column=button.column)
+    window.bind(button.key, (handle_operation(button.text) if button.is_operation else handle_number(button.text)))
 
 window.mainloop()
 
